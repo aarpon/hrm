4542d0f0119c95588c8aad6e85dbb41eca9f6128
Asheesh Gulati <asheesh.gulati@epfl.ch>
2007-07-09 12:06:10 +0000

    Apache needs access to filesystem commands

--- add_user_popup.php
+++ add_user_popup.php
@@ -104,7 +104,7 @@ if (isset($_POST['add'])) {
             $user->setEmail('');
             $user->setGroup('');
             $message = "            <p class=\"warning\">New user successfully added to the system</p>";
-            shell_exec("hrm2share create " . strtolower($_POST['username']));
+            shell_exec("bin/hrm create " . strtolower($_POST['username']));
             $added = True;
           }
           else $message = "            <p class=\"warning\">Database error, please inform the person in charge</p>";
