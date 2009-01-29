8017ed8677f2b64dc2b734bd218c546b841a4c1b
Aaron Ponti <aarpon@gmail.com>
2009-01-29 17:32:19 +0000

    Fixed css 2.1 compliance (and added Valid CSS! image in the footer).

--- footer.inc.php
+++ footer.inc.php
@@ -64,8 +64,9 @@
     
     <div id="validation">
         <a href="http://validator.w3.org/check?uri=referer" onclick="clean()">
-                <img src="images/valid-xhtml10.png" alt="Valid XHTML 1.0 Strict" height="31" width="88" />
+            <img style="border:0;width:88px;height:31px" src="images/valid-xhtml10.png" alt="Valid XHTML 1.0 Strict" />
         </a>
+        <img style="border:0;width:88px;height:31px" src="images/valid-css21.png" alt="Valid CSS!" />
     </div>
     
 </div> <!-- basket -->
