df3c0dbfb713486b0b0663d345f448bb60076f51
Aaron Ponti <aarpon@gmail.com>
2009-02-20 13:08:55 +0000

    Increased the size of the select PSF popup window to accomodate the text better.

--- scripts/settings.js
+++ scripts/settings.js
@@ -77,7 +77,7 @@ function fixGeometryAndChannels(geometry, channels) {
 function seek(channel) {
     var url = "select_psf_popup.php?channel=" + channel;
     var name = "snitch";
-    var options = "directories = no, menubar = no, status = no, width = 560, height = 380";
+    var options = "directories = no, menubar = no, status = no, width = 560, height = 400";
     snitch = window.open(url, name, options);
     snitch.focus();
 }
