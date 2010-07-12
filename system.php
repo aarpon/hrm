2a9d33e89f9bbe528113a99f6c8c0945aafb3b75
Aaron Ponti <aarpon@gmail.com>
2010-07-12 16:54:03 +0000

    Added a new static class System that can be used to query for system versions, php.ini and hrm configuration variable values. The page system.php (accessible from the admin home page) uses it to display all information.

--- system.php
+++ system.php
@@ -3,13 +3,9 @@
 // Copyright and license notice: see license.txt
 
 require_once("./inc/User.inc");
-require_once("./inc/Util.inc");
-require_once("./inc/Database.inc");
-require_once("./inc/hrm_config.inc");
+require_once("./inc/System.inc");
 
 session_start();
-
-$db = new DatabaseConnection();
 
 if (isset($_GET['home'])) {
   header("Location: " . "home.php"); exit();
@@ -19,7 +15,7 @@ if (!isset($_SESSION['user']) || !$_SESSION['user']->isLoggedIn() || $_SESSION['
   header("Location: " . "login.php"); exit();
 }
 
-if (isset($_SERVER['HTTP_REFERER']) && !strstr($_SERVER['HTTP_REFERER'], 'account')) {
+if (isset($_SERVER['HTTP_REFERER'])) {
   $_SESSION['referer'] = $_SERVER['HTTP_REFERER'];
 }
 
@@ -38,126 +34,70 @@ include("header.inc.php");
     
     <div id="content">
 
-        <!-- // TODO: use css for styling tables below -->
-        
-        <h3>System summary</h3>
+      <h3>System summary</h3>
 
-        <h4>Huygens Core</h4>
-        <table style="font-size:75%">
-          <tbody>
-            <tr>
-              <td style="width:60%;text-align:left;">
-                HuCore version
-              </td>
-              <td style="width:40%;text-align:left;">
-                <?php echo getHuCoreVersionAsString( $db->getHuCoreVersion() ); ?>
-              </td>
-            </tr>
-          </tbody>
+      <div id="system">
+        <table>
+          <tr>
+            <td class="section">Huygens</td>
+            <td class="value">&nbsp;</td>
+          </tr>
+          <tr>
+            <td class="key">HuCore version</td>
+            <td class="value"><?php echo System::huCoreVersion(); ?></td>
+          </tr>
+          <tr>
+            <td class="section">LAMP versions</td>
+            <td class="value">&nbsp;</td>
+          </tr>
+          <tr>
+            <td class="key">Apache version</td>
+            <td class="value"><?php echo System::apacheVersion(); ?></td>
+          </tr>
+          <tr>
+            <td class="key">Database type and version</td>
+            <td class="value"><?php echo System::databaseType() . ' ' .
+              System::databaseVersion(); ?></td>
+          </tr>
+          <tr>
+            <td class="key">PHP (Apache mod) version</td>
+            <td class="value"><?php echo System::phpVersion(); ?></td>
+          </tr>
+          <tr>
+            <td class="section">Configuration</td>
+            <td class="value">&nbsp;</td>
+          </tr>
+          <tr>
+            <td class="key">Memory limit</td>
+            <td class="value"><?php echo System::memoryLimit(); ?></td>
+          </tr>
+          <tr>
+            <td class="key">Maximum post size (php.ini)</td>
+            <td class="value"><?php echo System::postMaxSizeFromIni(); ?></td>
+          </tr>
+          <tr>
+            <td class="key">Maximum post size (HRM configuration)</td>
+            <td class="value"><?php echo System::postMaxSizeFromConfig(); ?></td>
+          </tr>
+          <tr>
+            <td class="key">Maximum post size (in use)</td>
+            <td class="value"><?php echo System::postMaxSize(); ?></td>
+          </tr>
+          <tr>
+            <td class="key">Maximum upload file size (php.ini)</td>
+            <td class="value"><?php echo System::uploadMaxFileSizeFromIni(); ?></td>
+          </tr>
+          <tr>
+            <td class="key">Maximum upload file size (HRM configuration)</td>
+            <td class="value"><?php echo System::uploadMaxFileSizeFromConfig(); ?></td>
+          </tr>
+          <tr>
+            <td class="key">Maximum upload file size (in use)</td>
+            <td class="value"><?php echo System::uploadMaxFileSize(); ?></td>
+          </tr>
         </table>
-        
-        <h4>Database</h4>
-        <table style="font-size:75%">
-          <tbody>
-            <tr>
-              <td style="width:60%;text-align:left;">
-                Database type
-              </td>
-              <td style="width:40%;text-align:left;">
-                <?php echo $db->type(); ?>
-              </td>
-            </tr>
-            <tr>
-              <td style="width:60%;text-align:left;">
-                Database version
-              </td>
-              <td style="width:40%;text-align:left;">
-                <?php echo $db->version(); ?>
-              </td>
-            </tr>
-          </tbody>
-        </table>
-
-        <h4>PHP</h4>
-        <table style="font-size:75%">
-          <tbody>
-            <tr>
-              <td style="width:60%;text-align:left;">
-                PHP version
-              </td>
-              <td style="width:40%;text-align:left;">
-                <?php echo phpversion( ); ?>
-              </td>
-            </tr>
-          </tbody>
-        </table>
-
-        <h4>Memory and file sizes</h4>
-        <table style="font-size:75%">
-          <tbody>
-            <tr>
-              <td style="width:60%;text-align:left;">
-                Memory limit (php.ini)
-              </td>
-              <td style="width:40%;text-align:left;">
-                <?php echo ini_get( 'memory_limit' ); ?>
-              </td>
-            </tr>
-            <tr>
-              <td style="width:60%;text-align:left;">
-                Maximum post size (php.ini)
-              </td>
-              <td style="width:40%;text-align:left;">
-                <?php echo ini_get( 'post_max_size' ); ?>
-              </td>
-            </tr>
-            <tr>
-              <td style="width:60%;text-align:left;">
-                Maximum post size (configuration)
-              </td>
-              <td style="width:40%;text-align:left;">
-                <?php
-                if ( isset( $max_post_limit ) ) {
-                  if ( $max_post_limit == 0 ) {
-                    echo "Limited by php.ini.";
-                  } else {
-                    echo $max_post_limit . "M";
-                  }
-                } else {
-                  echo "Not defined!";
-                }
-                ?>
-              </td>
-            </tr>
-            <tr>
-              <td style="width:60%;text-align:left;">
-                Maximum upload file size (php.ini)
-              </td>
-              <td style="width:40%;text-align:left;">
-                <?php echo ini_get( 'upload_max_filesize' ); ?>
-              </td>
-            </tr>
-            <tr>
-              <td style="width:60%;text-align:left;">
-                Maximum post size (configuration)
-              </td>
-              <td style="width:40%;text-align:left;">
-                <?php
-                if ( isset( $max_upload_limit ) ) {
-                  if ( $max_upload_limit == 0 ) {
-                    echo "Limited by php.ini.";
-                  } else {
-                    echo $max_upload_limit . "M";
-                  }
-                } else {
-                  echo "Not defined!";
-                }
-                ?>
-              </td>
-            </tr>
-          </tbody>
-        </table>
-
+      </div> <!-- system -->
+      
     </div> <!-- content -->
     
     <div id="rightpanel">
