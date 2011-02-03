4777711e4d3c68a8241003280c6406d248179cd3
Aaron Ponti <aarpon@gmail.com>
2011-02-03 17:22:48 +0000

    Added minimum Hucore version needed to run current HRM version (in System.inc) and corresponding check (in login.php).

--- login.php
+++ login.php
@@ -24,27 +24,27 @@ global $authenticateAgainst;
  *
  */
 
-  // Here we store the cleaned variables
-  $clean = array(
+// Here we store the cleaned variables
+$clean = array(
     "username" => "",
     "password" => "" );
-  
-  // Username
-  if ( isset( $_POST["username"] ) ) {
-    if ( Validator::isUsernameValid( $_POST["username"] ) ) {
-      $clean["username"] = $_POST["username"];       
-    }
-  }
 
-  // Password
-  if ( isset( $_POST["password"] ) ) {
-    if ( Validator::isPasswordValid( $_POST["password"] ) ) {
-      $clean["password"] = $_POST["password"];       
-    }
-  }
-  
-  // TODO Clean $_POST['request']
- 
+// Username
+if ( isset( $_POST["username"] ) ) {
+	if ( Validator::isUsernameValid( $_POST["username"] ) ) {
+		$clean["username"] = $_POST["username"];
+	}
+}
+
+// Password
+if ( isset( $_POST["password"] ) ) {
+	if ( Validator::isPasswordValid( $_POST["password"] ) ) {
+		$clean["password"] = $_POST["password"];
+	}
+}
+
+// TODO Clean $_POST['request']
+
 /*
  *
  * END OF SANITIZE INPUT
@@ -70,174 +70,184 @@ session_destroy();
 session_start();
 
 if ( isset( $_POST['password'] ) && isset( $_POST['username'] ) ) {
-  if ( $clean['password'] != "" && $clean['username'] != "" ) {
-    
-    // Create a user
-    $tentativeUser = new User();
-    $tentativeUser ->setName($clean['username']);
-    $tentativeUser ->logOut(); // TODO
-  
-    if ($tentativeUser->logIn($clean['username'], $clean['password'], $_SERVER['REMOTE_ADDR'])) {
-      if ($tentativeUser ->isLoggedIn()) {
-        // Make sure that the user source and destination folders exist
-    		{
-          $fileServer = new FileServer( $clean['username'] );
-          if ( $fileServer->isReachable() == false ) {
-            shell_exec("bin/hrm create " . $clean['username'] );
-            }
-          }
-        
-          // TODO unregister also "setting" and "task_setting"
-          unset($_SESSION['editor']);
-        
-          // Register the user in the session
-          $_SESSION['user'] = $tentativeUser;
-          
-          if ( $authenticateAgainst == "MYSQL" ) {
-            if ( $req != "" ) {
-              header("Location: " . $req);
-              exit();
-            } else {
-              // Proceed to home
-              header("Location: " . "home.php");
-              exit();
-            }
-          } else {
-            // Alternative authentication: proceed to home
-            header("Location: " . "home.php");
-            exit();
-          }
-        }
-      } else if ( $tentativeUser->isLoginRestrictedToAdmin() ) {
-        if ( ( $tentativeUser->isAdmin() ) && ( $tentativeUser->exists() ) ) {
-          $message = "            <p class=\"warning\">Wrong password.</p>\n";
-        } else {
-          $message = "            <p class=\"warning\">Only the administrator is allowed to login in order to perform maintenance.</p>\n";
-        }
-      } else {
-        if ( $tentativeUser->isSuspended()) {
-          $message = "            <p class=\"warning\">Your account has been suspended, please contact the administrator.</p>\n";
-        } else {
-          $message = "            <p class=\"warning\">Sorry, wrong user name or password.</p>\n";
-        }
-      }
-    } else {
-      $message = "            <p class=\"warning\">Sorry, invalid user name or password.</p>\n";
-    }
-  }
-  
+	if ( $clean['password'] != "" && $clean['username'] != "" ) {
+
+		// Create a user
+		$tentativeUser = new User();
+		$tentativeUser ->setName($clean['username']);
+		$tentativeUser ->logOut(); // TODO
+
+		if ($tentativeUser->logIn($clean['username'], $clean['password'], $_SERVER['REMOTE_ADDR'])) {
+			if ($tentativeUser ->isLoggedIn()) {
+				// Make sure that the user source and destination folders exist
+				{
+					$fileServer = new FileServer( $clean['username'] );
+					if ( $fileServer->isReachable() == false ) {
+						shell_exec("bin/hrm create " . $clean['username'] );
+					}
+				}
+
+				// TODO unregister also "setting" and "task_setting"
+				unset($_SESSION['editor']);
+
+				// Register the user in the session
+				$_SESSION['user'] = $tentativeUser;
+
+				if ( $authenticateAgainst == "MYSQL" ) {
+					if ( $req != "" ) {
+						header("Location: " . $req);
+						exit();
+					} else {
+						// Proceed to home
+						header("Location: " . "home.php");
+						exit();
+					}
+				} else {
+					// Alternative authentication: proceed to home
+					header("Location: " . "home.php");
+					exit();
+				}
+			}
+		} else if ( $tentativeUser->isLoginRestrictedToAdmin() ) {
+			if ( ( $tentativeUser->isAdmin() ) && ( $tentativeUser->exists() ) ) {
+				$message = "            <p class=\"warning\">Wrong password.</p>\n";
+			} else {
+				$message = "            <p class=\"warning\">Only the administrator is allowed to login in order to perform maintenance.</p>\n";
+			}
+		} else {
+			if ( $tentativeUser->isSuspended()) {
+				$message = "            <p class=\"warning\">Your account has been suspended, please contact the administrator.</p>\n";
+			} else {
+				$message = "            <p class=\"warning\">Sorry, wrong user name or password.</p>\n";
+			}
+		}
+	} else {
+		$message = "            <p class=\"warning\">Sorry, invalid user name or password.</p>\n";
+	}
+}
+
 include("header.inc.php");
 
 ?>
 
-	<div id="nav">
-		<ul>
-			<li><a href="javascript:openWindow('http://huygens-rm.org/wiki/index.php?title=Changelog')"><img src="images/whatsnew.png" alt="website" />&nbsp;What's new?</a></li>
-			<li><a href="javascript:openWindow('http://www.huygens-rm.org')"><img src="images/logo_small.png" alt="website" />&nbsp;Website</a></li>
-			<li><a href="javascript:openWindow('http://support.svi.nl/wiki')"><img src="images/wiki.png" alt="website" />&nbsp;SVI wiki</a></li>
-			<li><a href="javascript:openWindow('http://support.svi.nl/wiki/style=hrm&amp;help=HuygensRemoteManagerHelpLogin')"><img src="images/help.png" alt="help" />&nbsp;Help</a></li> 
-		</ul>
-	</div>
-	
-	<div id="content">
-	
-		<?php
-		  // Check that the database is reachable
-		  $db   = new DatabaseConnection( );
-		  if ( !$db->isReachable( ) ) {
-  			echo "<div class=\"dbOutDated\">Warning: the database is not reachable!\n";
-  			echo "<p>Please contact your administrator.</p>".
+<div id="nav">
+<ul>
+	<li><a
+		href="javascript:openWindow('http://huygens-rm.org/wiki/index.php?title=Changelog')"><img
+		src="images/whatsnew.png" alt="website" />&nbsp;What's new?</a></li>
+	<li><a href="javascript:openWindow('http://www.huygens-rm.org')"><img
+		src="images/logo_small.png" alt="website" />&nbsp;Website</a></li>
+	<li><a href="javascript:openWindow('http://support.svi.nl/wiki')"><img
+		src="images/wiki.png" alt="website" />&nbsp;SVI wiki</a></li>
+	<li><a
+		href="javascript:openWindow('http://support.svi.nl/wiki/style=hrm&amp;help=HuygensRemoteManagerHelpLogin')"><img
+		src="images/help.png" alt="help" />&nbsp;Help</a></li>
+</ul>
+</div>
+
+<div id="content"><?php
+// Check that the database is reachable
+$db   = new DatabaseConnection( );
+if ( !$db->isReachable( ) ) {
+	echo "<div class=\"dbOutDated\">Warning: the database is not reachable!\n";
+	echo "<p>Please contact your administrator.</p>".
   				  "<p>You will not be allowed to login " .
   				  "until this issue has been fixed.</p></div>";
-  			echo "</div>\n";
-  			include("footer.inc.php");
-  			return;
-		  }
- 		  // Check that the hucore version is known
-		  if ( System::huCoreVersion( ) == 0 ) {
-  			echo "<div class=\"dbOutDated\">Warning: unknown HuCore version!\n";
-  			echo "<p>Please ask the administrator to start the queue manager.<p>" . 
+	echo "</div>\n";
+	include("footer.inc.php");
+	return;
+}
+// Check that the hucore version is known
+if ( System::huCoreVersion( ) == 0 ) {
+	echo "<div class=\"dbOutDated\">Warning: unknown HuCore version!\n";
+	echo "<p>Please ask the administrator to start the queue manager.</p>" .
   				"<p>You are now allowed to login until this issue has been fixed.</p></div>";
-  			echo "</div>\n";
-  			include("footer.inc.php");
-  			return;
-		  }  
-		  // Check that the database is up-to-date
-		  if ( System::isDBUpToDate( ) == false ) {
-  			echo "<div class=\"dbOutDated\">Warning: the database is not up-to-date!\n";
-  			echo "<p>This happens if HRM was recently updated but the " . 
+	echo "</div>\n";
+	include("footer.inc.php");
+	return;
+}
+// Check that hucore is recent enough to run this version of the HRM
+if ( System::isMinHuCoreVersion( ) == false ) {
+	echo "<div class=\"dbOutDated\">Warning: you need at least HuCore version " .
+	System::minHuCoreVersion() . " for HRM " . System::getHRMVersion() . "!\n";
+	echo "<p>Please contact the administrator.</p></div>";
+	echo "</div>\n";
+	include("footer.inc.php");
+	return;
+}
+// Check that the database is up-to-date
+if ( System::isDBUpToDate( ) == false ) {
+	echo "<div class=\"dbOutDated\">Warning: the database is not up-to-date!\n";
+	echo "<p>This happens if HRM was recently updated but the " .
   				  "database was not. You are now allowed to login " .
   				  "until this issue has been fixed.</p>";
-  			echo "<p>Only the administrator can login.</p></div>";
-		  }
-		?>
-		<h2>Welcome</h2>
-		
-		<p class="intro">
-		The <a href="javascript:openWindow('http://hrm.sourceforge.net')">Huygens
-		Remote Manager</a> is an easy to use interface to the
-			Huygens Software by 
-			<a href="javascript:openWindow('http://www.svi.nl')">Scientific 
-			Volume Imaging B.V.</a> that allows for multi-user, large-scale deconvolution.
-		</p>
-		
-		<div id="logos">
-			<div class="logo-fmi">
-				<a href="javascript:openWindow('http://www.fmi.ch')"><img src="images/logo_fmi.png" alt="FMI" /></a>
-				<p>Friedrich Miescher Institute</p>
-				<p><a href="javascript:openWindow('http://www.fmi.ch/faim')">Facility for Advanced Imaging and Microscopy</a></p>
-			</div>
-			<div class="logo-mri">
-				<a href="javascript:openWindow('http://www.mri.cnrs.fr')"><img src="images/logo_mri.png" alt="MRI" /></a>
-				<p>Montpellier RIO Imaging</p>
-			</div>
-			<div class="logo-epfl">
-				<a href="javascript:openWindow('http://www.epfl.ch')"><img src="images/logo_epfl.png" alt="EPFL" /></a>
-				<p>Federal Institute of Technology - Lausanne</p>
-				<p><a href="javascript:openWindow('http://biop.epfl.ch')">BioImaging and Optics platform</a></p>
-			</div>
-		</div>
+	echo "<p>Only the administrator can login.</p></div>";
+}
+?>
+<h2>Welcome</h2>
 
-	</div> <!-- content -->
-	
-	<div id="rightpanel">
-	<p />    
-	<div id="login">
-			<form method="post" action="">
-				<fieldset>
-					<legend>
-						<a href="javascript:openWindow('http://support.svi.nl/wiki/style=hrm&amp;help=HuygensRemoteManagerHelpLogin')"><img src="images/help.png" alt="?"/></a>
-						Login
-					</legend>
-					<?php
-					if ( $authenticateAgainst == "MYSQL" ) {
-					  $login_message = "<p class=\"expl\">If you do not have an account, please register <a href=\"registration.php\">here</a>.</p>";
-					} else {
-					  $login_message = "<p class=\"expl\">Please enter your credentials.</p>";
-					}
-					echo $login_message;
-					?>
-					<label for="username">Username:</label>
-					<input id="username" name="username" type="text" class="textfield" tabindex="1" />
-					<br />
-					<label for="password">Password:</label>
-					<input id="password" name="password" type="password" class="textfield" tabindex="2" />
-					<br />
-					<input type="hidden" name="request" value="<?php echo $req?>" />
-					<input type="submit" class="button" value="login" />
-				</fieldset>
-			</form>
-		</div>
-		
-		<div id="message">
-<?php
+<p class="intro">The <a
+	href="javascript:openWindow('http://hrm.sourceforge.net')">Huygens
+Remote Manager</a> is an easy to use interface to the Huygens Software
+by <a href="javascript:openWindow('http://www.svi.nl')">Scientific
+Volume Imaging B.V.</a> that allows for multi-user, large-scale
+deconvolution.</p>
+
+<div id="logos">
+<div class="logo-fmi"><a
+	href="javascript:openWindow('http://www.fmi.ch')"><img
+	src="images/logo_fmi.png" alt="FMI" /></a>
+<p>Friedrich Miescher Institute</p>
+<p><a href="javascript:openWindow('http://www.fmi.ch/faim')">Facility
+for Advanced Imaging and Microscopy</a></p>
+</div>
+<div class="logo-mri"><a
+	href="javascript:openWindow('http://www.mri.cnrs.fr')"><img
+	src="images/logo_mri.png" alt="MRI" /></a>
+<p>Montpellier RIO Imaging</p>
+</div>
+<div class="logo-epfl"><a
+	href="javascript:openWindow('http://www.epfl.ch')"><img
+	src="images/logo_epfl.png" alt="EPFL" /></a>
+<p>Federal Institute of Technology - Lausanne</p>
+<p><a href="javascript:openWindow('http://biop.epfl.ch')">BioImaging and
+Optics platform</a></p>
+</div>
+</div>
+
+</div>
+<!-- content -->
+
+<div id="rightpanel">
+<p />
+<div id="login">
+<form method="post" action="">
+<fieldset><legend> <a
+	href="javascript:openWindow('http://support.svi.nl/wiki/style=hrm&amp;help=HuygensRemoteManagerHelpLogin')"><img
+	src="images/help.png" alt="?" /></a> Login </legend> <?php
+	if ( $authenticateAgainst == "MYSQL" ) {
+		$login_message = "<p class=\"expl\">If you do not have an account, please register <a href=\"registration.php\">here</a>.</p>";
+	} else {
+		$login_message = "<p class=\"expl\">Please enter your credentials.</p>";
+	}
+	echo $login_message;
+	?> <label for="username">Username:</label> <input id="username"
+	name="username" type="text" class="textfield" tabindex="1" /> <br />
+<label for="password">Password:</label> <input id="password"
+	name="password" type="password" class="textfield" tabindex="2" /> <br />
+<input type="hidden" name="request" value="<?php echo $req?>" /> <input
+	type="submit" class="button" value="login" /></fieldset>
+</form>
+</div>
+
+<div id="message"><?php
 
 echo $message;
 
-?>
-		</div>
-	 
-	</div> <!-- rightpanel -->
+?></div>
+
+</div>
+<!-- rightpanel -->
 
 <?php
 
