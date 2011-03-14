1e5fbf1d360da9ab395c8bf7b9da865bfe611aca
Aaron Ponti <aarpon@gmail.com>
2011-03-14 17:47:05 +0000

    If at least one of the refractive indices is not known, we cannot calculate whether an aberration correction is necessary and we leave the decision to the user in the aberration_correction.php page.

--- capturing_parameter.php
+++ capturing_parameter.php
@@ -91,26 +91,36 @@ if ($PSF == 'measured' ) {
 
   // Calculate the deviation
   if ( ( $sampleRI == 0 ) ||  ( $objectiveRI == 0 ) ) {
-	$deviation = 0;
-  } else {
-	$deviation = abs( $sampleRI - $objectiveRI ) / $objectiveRI;
-  }
-              
-  // Do we need to go to the aberration correction page? We 
-  if ( $deviation < 0.01 ) {
-    $pageToGo = 'select_parameter_settings.php';
-    $controlIconToShow = "icon save";
-    $controlTextToShow = "Save and return to the image parameters selection page.";
-    $saveParametersToDB = true;
-    // Make sure to turn off the correction
-    $_SESSION['setting']->parameter( 'AberrationCorrectionNecessary' )->setValue( '0' );
-    $_SESSION['setting']->parameter( 'PerformAberrationCorrection' )->setValue( '0' );
-  } else {
-    $pageToGo = 'aberration_correction.php';
+	// If at least one of the refractive indices is not known, we cannot
+	// calculate whether an aberration correction is necessary and we leave
+	// the decision to the user in the aberration_correction.php page.
+	$pageToGo = 'aberration_correction.php';
     $controlIconToShow = "icon next";
     $controlTextToShow = "Continue to next page.";
     $_SESSION['setting']->parameter( 'AberrationCorrectionNecessary' )->setValue( '1' );
     $saveParametersToDB = false;
+  } else {
+	// If we know both the refractive indices we can calculate the deviation
+	// and skip the aberration correction page in case the deviation is smaller
+	// than 1%.
+	$deviation = abs( $sampleRI - $objectiveRI ) / $objectiveRI;
+          
+	// Do we need to go to the aberration correction page?
+	if ( $deviation < 0.01 ) {
+	  $pageToGo = 'select_parameter_settings.php';
+	  $controlIconToShow = "icon save";
+	  $controlTextToShow = "Save and return to the image parameters selection page.";
+	  $saveParametersToDB = true;
+	  // Make sure to turn off the correction
+	  $_SESSION['setting']->parameter( 'AberrationCorrectionNecessary' )->setValue( '0' );
+	  $_SESSION['setting']->parameter( 'PerformAberrationCorrection' )->setValue( '0' );
+	} else {
+	  $pageToGo = 'aberration_correction.php';
+	  $controlIconToShow = "icon next";
+	  $controlTextToShow = "Continue to next page.";
+	  $_SESSION['setting']->parameter( 'AberrationCorrectionNecessary' )->setValue( '1' );
+	  $saveParametersToDB = false;
+	}
   }
 }
 
