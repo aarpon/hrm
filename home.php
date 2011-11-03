2ec6044ffc2c432886d907c60b2775f0e9708e5e
Aaron Ponti <aarpon@gmail.com>
2011-11-03 14:02:27 +0000

    Changed home page link 'Manual' to 'User manual'.

--- home.php
+++ home.php
@@ -42,7 +42,7 @@ include("header.inc.php");
                 <a href="javascript:openWindow(
                    'http://huygens-rm.org/home/?q=node/7')">
                     <img src="images/manual.png" alt="manual" />
-                    &nbsp;Manual
+                    &nbsp;User manual
                 </a>
             </li>
 			<li>
