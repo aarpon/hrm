c49fbf1e4eccbcbd7c90f8e2740fa18958adabc2
Niko Ehrenfeuchter <mail@he1ix.org>
2013-07-06 01:50:55 +0200

    Monitor and dispatch jobs from a spool directory via pyinotify.
    This is the first and very rudimentary working version of a Queue Manager using
    an extremely simple sequential processing model on a single GC3Pie resource.
    Jobs are submitted by placing new files in a spool directory where they get
    picked up by the Queue Manager.
    
    Closes #133
    
    For complaints, please refer to xkcd:323.
    
    (cherry picked from commit 829041b3fb92711179f204dfb79f24540a1bdf74)

--- run/HRM_QueueManager.py
+++ run/HRM_QueueManager.py
@@ -7,8 +7,11 @@ The prototype of a new GC3Pie-based Queue Manager for HRM.
 """
 
 # TODO:
-# - monitor a "new" directory via pyinotify
-# - add them to the queue
+# - move processed jobfiles to cur/done
+# - let gc3pie decide when to dispatch a job (currently the call to run_job()
+#   is blocking and thus the whole thing is limited to single sequential job
+#   instances, even if more resources were available
+# - create a class for the jobparser stuff
 
 # stdlib imports
 import sys
@@ -18,6 +21,7 @@ import time
 import gc3libs
 
 import ConfigParser
+import pyinotify
 import argparse
 import pprint
 
@@ -97,6 +101,16 @@ def parse_jobfile(name):
     return job
 
 
+class EventHandler(pyinotify.ProcessEvent):
+    def __init__(self, joblist):
+        self.joblist = joblist
+
+    def process_IN_CREATE(self, event):
+        warn("Found new jobfile '%s', processing..." % event.pathname)
+        self.joblist.append(parse_jobfile(event.pathname))
+        warn(self.joblist)
+
+
 class HucoreDeconvolveApp(gc3libs.Application):
     """
     This application calls `hucore` with a given template file and retrives the
@@ -152,8 +166,8 @@ def run_job(engine, job):
 
 def main():
     argparser = argparse.ArgumentParser(description=__doc__)
-    argparser.add_argument('-j', '--jobfile', required=True,
-        help='job description file in ConfigParser format')
+    argparser.add_argument('-s', '--spooldir', required=True,
+        help='spooling directory for new jobfiles')
     argparser.add_argument('-r', '--resource', required=False,
         help='GC3Pie resource name')
     try:
@@ -161,8 +175,14 @@ def main():
     except IOError as e:
         argparser.error(str(e))
 
-    job = parse_jobfile(args.jobfile)
+    joblist = []
 
+    wm = pyinotify.WatchManager() # Watch Manager
+    mask = pyinotify.IN_CREATE # watched events
+    notifier = pyinotify.ThreadedNotifier(wm, EventHandler(joblist))
+    notifier.start()
+    watchdir = args.spooldir
+    wdd = wm.add_watch(watchdir, mask, rec=False)
 
     warn('Creating an instance of a GC3Pie engine using the configuration '
         'file present in your home directory.')
@@ -171,7 +191,21 @@ def main():
     if args.resource:
         engine.select_resource(args.resource)
 
-    run_job(engine, job)
+    print('Added watch to "%s", press Ctrl-C to abort.' % watchdir)
+    # FIXME: Ctrl-C while a job is running leaves it alone (and thus as well
+    # the files transferred for / generated from processing)
+    while True:
+        try:
+            if len(joblist) > 0:
+                run_job(engine, joblist.pop(0))
+            time.sleep(1)
+        except KeyboardInterrupt:
+            break
+
+    print('Cleaning up.')
+    print(joblist)
+    wm.rm_watch(wdd.values())
+    notifier.stop()
 
 if __name__ == "__main__":
     sys.exit(main())
