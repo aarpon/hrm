53c7ce7fbad43e6d3f68d544daa5953145c5db15
Niko Ehrenfeuchter <mail@he1ix.org>
2013-11-06 00:28:34 +0100

    Update navbar for login page.

---
+++ inc/nav/hrm_project_website.inc.php
@@ -0,0 +1,6 @@
+<li>
+    <a href="javascript:openWindow('http://www.huygens-rm.org')">
+        <img src="images/logo_small.png" alt="website" />
+        &nbsp;HRM Project Website
+    </a>
+</li>
---
+++ inc/nav/whats_new.inc.php
@@ -0,0 +1,7 @@
+<li>
+    <a href="javascript:openWindow(
+       'http://huygens-rm.org/home/?q=node/27')">
+        <img src="images/whatsnew.png" alt="website" />
+        &nbsp;What's new?
+    </a>
+</li>
