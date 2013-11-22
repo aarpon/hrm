84949a7fdadd33155d77ae678b6717591abe17c3
Niko Ehrenfeuchter <mail@he1ix.org>
2013-11-22 16:52:28 +0100

    Add "Report an issue" button.
    Using the include-based navbar now.
    
    fixes #119

--- home.php
+++ home.php
@@ -39,6 +39,7 @@ include("header.inc.php");
             <?php
                 wiki_link('HuygensRemoteManagerHelpHome');
                 include("./inc/nav/manual.inc.php");
+                include("./inc/nav/report_issue.inc.php");
                 include("./inc/nav/check_updates.inc.php");
             ?>
         </ul>
