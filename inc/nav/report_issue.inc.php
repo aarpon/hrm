9c1a8ae2bd673b725771c0e61ad0d6f3933d4797
Niko Ehrenfeuchter <mail@he1ix.org>
2013-11-22 16:33:02 +0100

    Update "Report issue" link + icon.
    So it matches what Aaron created.
    
    refs #119

--- inc/nav/report_issue.inc.php
+++ inc/nav/report_issue.inc.php
@@ -1,7 +1,6 @@
 <li>
-    <a href="javascript:openWindow(
-        'http://hrm.svi.nl:8080/redmine/projects/public/issues/new')">
-        <!--img src="images/help.png" alt="help" /-->
-        &nbsp;Report issue
+    <a href="http://hrm.svi.nl:8080/redmine/projects/public/issues/new" target="_blank">
+        <img src="images/bug.png" alt="Bug report" />
+        &nbsp;Report an issue
     </a>
 </li>
