a89407ae1e8a551cbcc30119bacc6aa8ce4ec442
Niko Ehrenfeuchter <mail@he1ix.org>
2013-11-25 17:07:30 +0100

    Remove unneeded Javascript, use "target" attribute instead.
    fixes #206

--- inc/nav/hrm_project_website.inc.php
+++ inc/nav/hrm_project_website.inc.php
@@ -1,5 +1,5 @@
 <li>
-    <a href="javascript:openWindow('http://www.huygens-rm.org')">
+    <a href="http://www.huygens-rm.org" target="_blank">
         <img src="images/logo_small.png" alt="website" />
         &nbsp;HRM Project Website
     </a>
--- inc/nav/manual.inc.php
+++ inc/nav/manual.inc.php
@@ -1,5 +1,5 @@
 <li>
-    <a href="javascript:openWindow('http://huygens-rm.org/home/?q=node/7')">
+    <a href="http://huygens-rm.org/home/?q=node/7" target="_blank">
         <img src="images/manual.png" alt="manual" />&nbsp;User manual
     </a>
 </li>
