2e3dcb24629bc03e54de13b9a6b6740f51246967
Niko Ehrenfeuchter <mail@he1ix.org>
2013-11-25 16:41:12 +0100

    Add get_wiki_link(), turn wiki_link() into a wrapper.

--- inc/wiki_help.inc.php
+++ inc/wiki_help.inc.php
@@ -9,13 +9,29 @@
 
   \param  $pagename  The title of the page in the SVI wiki.
   \param  $linkname  The string used for the link in the HTML.
+
+  \return $html  A string containing the HTML code.
+ */
+function get_wiki_link($pagename, $linkname="Help") {
+        $html = '<li>';
+        $html .= '<a href="http://www.svi.nl/' . $pagename . '" target="_blank">';
+        $html .= '<img src="images/help.png" alt="help" />';
+        $html .= '&nbsp;' . $linkname;
+        $html .= '</a>';
+        $html .= '</li>';
+        return $html;
+}
+
+/*!
+  \brief Print HTML code to link to a specific page in the SVI wiki.
+
+  A wrapper function for get_wiki_link() that directly prints the generated HTML
+  code.
+
+  \param  $pagename  The title of the page in the SVI wiki.
+  \param  $linkname  The string used for the link in the HTML.
  */
 function wiki_link($pagename, $linkname="Help") {
-        echo '<li>';
-        echo '<a href="http://www.svi.nl/' . $pagename . '" target="_blank">';
-        echo '<img src="images/help.png" alt="help" />';
-        echo '&nbsp;' . $linkname;
-        echo '</a>';
-        echo '</li>';
+        echo get_wiki_link($pagename, $linkname);
 }
 ?>
