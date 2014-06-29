f0dd320747c3fd70208c67518bbc67bcb0f73203
Niko Ehrenfeuchter <nikolaus.ehrenfeuchter@unibas.ch>
2014-06-29 20:35:01 +0200

    More testing with different queue categories (users).
    refs #135, #273

--- run/tests/test_HRM_JobQueue.py
+++ run/tests/test_HRM_JobQueue.py
@@ -17,30 +17,47 @@ except ImportError:
 # during a single IPython session:
 reload(HRM)
 
-job1 = HRM.JobDescription('spool/new/job1.cfg', 'file')
-job2 = HRM.JobDescription('spool/new/job1.cfg', 'file')
-job3 = HRM.JobDescription('spool/new/job1.cfg', 'file')
+jobs = list(xrange(7))
 
-job1['uid'] = 'aaa'
-job2['uid'] = 'bbb'
-job3['uid'] = 'ccc'
+jobfile = 'spool/examples/deconvolution_job.cfg'
+for i in xrange(7):
+    jobs[i] = HRM.JobDescription(jobfile, 'file')
+
+jobs[0]['uid'] = 'aaa'
+jobs[1]['uid'] = 'bbb'
+jobs[2]['uid'] = 'ccc'
+
+jobs[3]['uid'] = 'ddd'
+jobs[3]['user'] = 'foo'
+jobs[4]['uid'] = 'eee'
+jobs[4]['user'] = 'foo'
+jobs[5]['uid'] = 'fff'
+jobs[5]['user'] = 'foo'
+jobs[6]['uid'] = 'ggg'
+jobs[6]['user'] = 'foo'
 
 jq = HRM.JobQueue()
-jq.append(job1)
-jq.append(job2)
-jq.append(job3)
 
-jq.pop()
-jq.pop()
-jq.pop()
-jq.pop()
+print("\n******** adding jobs to queue: ********")
+for job in jobs:
+    jq.append(job)
 
-jq.append(job1)
-jq.append(job2)
-jq.append(job3)
+print("\n******** retrieving jobs from queue: ********")
+for job in jobs:
+    jq.pop()
 
+
+print("\n******** adding jobs to queue: ********")
+for job in jobs:
+    jq.append(job)
+
+print("\n******** removing jobs from queue: ********")
 jq.remove('aaa')
 jq.remove('bbb')
+jq.pop()
+jq.remove('ggg')
+jq.pop()
 jq.remove('ccc')
+jq.remove('fff')
 jq.remove('aaa')
 
