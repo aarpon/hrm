c3980cc6b878f6abe7c63cfda9706f847a7f2ec7
Niko Ehrenfeuchter <nikolaus.ehrenfeuchter@unibas.ch>
2015-05-13 18:03:24 +0200

    Stop if selected node is of any other type than 'Image' or 'Dataset'.
    refs #443

--- scripts/omero.js
+++ scripts/omero.js
@@ -41,8 +41,9 @@ function omeroTransfer(form, fileSelection, browseFolder) {
             var dataset = image.parent;
         } else if (node.class = 'Dataset') {
             var dataset = node;
+        } else {
+            return false;
         }
-
             form.OmeDatasetId.value = dataset.id;
     }
 
