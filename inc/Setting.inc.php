592982b0ef26085fdb62d9ece74de1f5a8525859
danielsevilla <daniel@svi.nl>
2015-07-17 09:49:51 +0200

    Begin work on checking the posted chromatic aberration parameters.

--- inc/Setting.inc.php
+++ inc/Setting.inc.php
@@ -437,12 +437,6 @@ class ParameterSetting extends Setting {
             }
         }
 
-        if ($ok) {
-            if (!$this->checkPostedAberrationCorrectionParameters($postedParams)) {
-                $ok = False;
-            }
-        }
-
         if ( !$ok ) {
             $this->message  = "The selected parameter set contains empty values ";
             $this->message .= "which the $imageFormat format misses in its ";
@@ -1181,29 +1175,6 @@ class ParameterSetting extends Setting {
         return $noErrorsFound;
     }
 
-    /*!
-      \brief	Checks that the posted Aberration Correction Parameters are all
-              defined and valid
-      \param	$postedParameters	The $_POST array
-      \return	true if all Paraneters are defined and valid, false otherwise
-    */
-    public function checkPostedAberrationCorrectionParameters($postedParameters) {
-
-        /* if (count($postedParameters) == 0) { */
-        /*     $this->message = ''; */
-        /*     return False; */
-        /* } */
-
-        $this->message = '';
-        $noErrorsFound = True;
-
-        // PerformAberrationCorrection
-
-
-        
-
-        return $noErrorsFound;
-    }
 
     /*!
       \brief	Checks that the posted Calculate Pixel Size Parameters are all defined
@@ -2295,6 +2266,26 @@ class TaskSetting extends Setting {
         return $noErrorsFound;
     }
 
+    /*!
+      \brief	Checks that the posted Aberration Correction Parameters are all
+              defined and valid
+      \param	$postedParameters	The $_POST array
+      \return	true if all Paraneters are defined and valid, false otherwise
+    */
+    public function checkPostedChromaticAberrationParameters($postedParameters) {
+
+        if (count($postedParameters) == 0) {
+            $this->message = '';
+            return False;
+        }
+
+        $this->message = '';
+        $noErrorsFound = True;
+
+        return $noErrorsFound;
+    }
+    
+    
     /*!
       \brief	Returns all Task Parameter names
       \return array of Task Parameter names
