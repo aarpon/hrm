357e2db26d33385703d75f3d2d7fb6dc0e843818
Niko Ehrenfeuchter <nikolaus.ehrenfeuchter@unibas.ch>
2015-12-09 17:41:48 +0100

    Allow a custom favicon to be used.
    (cherry picked from commit:15b394eb9ea5a9e3415b7d941ce6cfa231f8f804)

--- header.inc.php
+++ header.inc.php
@@ -24,7 +24,13 @@ if (using_IE()) {
 ?>
 
   <title>Huygens Remote Manager</title>
-    <link rel="SHORTCUT ICON" href="images/hrm.ico"/>
+<?php
+    $ico = 'images/hrm_custom.ico';
+    if ! (file_exists($ico)) {
+        $ico = 'images/hrm.ico';
+    }
+    echo '    <link rel="SHORTCUT ICON" href="' . $ico . '"/>';
+?>
     <link rel="stylesheet" href="scripts/jqTree/jqtree.css">
     <link rel="stylesheet" href="scripts/jquery-ui/jquery-ui-1.9.1.custom.css">
 
