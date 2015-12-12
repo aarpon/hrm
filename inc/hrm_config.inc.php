86d3a9f9560eda72655281f96adcfd5ce95df10e
Niko Ehrenfeuchter <nikolaus.ehrenfeuchter@unibas.ch>
2015-12-12 23:47:50 +0100

    Remove deprecated 'change_ownership' feature.
    (Cherry-picked from 2e4c7a7, refs #447)

--- inc/hrm_config.inc.php
+++ inc/hrm_config.inc.php
@@ -20,19 +20,11 @@ if ($isServer == true) {
 
 // This is a hidden parameter for advanced users
 if (!isset($userManagerScript)) {
-
-    // This is a temporary hack to restore the pre-HRM 3.2.0 functionality
-    // that will be removed in 3.3.0. In case the administrator defined a
-    // custom user management script in the configuration files, we do not
-    // replace it -- no matter what the value of $change_ownership is!
-    if (isset($change_ownership) && $change_ownership == true) {
-        $userManagerScript = dirname(__FILE__) . "/../bin/hrm_user_manager_old";
-    } else {
-        // This default user manager script can be replaced in the configuration.
-        // A demo server, for example, may use links to a demo directory instead of
-        // creating an empty directory for each new user.
-        $userManagerScript = dirname(__FILE__) . "/../bin/hrm_user_manager";
-    }
+    
+    // This default user manager script can be replaced in the configuration.
+    // A demo server, for example, may use links to a demo directory instead of
+    // creating an empty directory for each new user.
+    $userManagerScript = dirname(__FILE__) . "/../bin/hrm_user_manager";
 }
 
 ?>
