3b177916be25480c9a37b60bb8fc0a13a30169d2
Niko Ehrenfeuchter <nikolaus.ehrenfeuchter@unibas.ch>
2016-01-27 10:55:46 +0100

    Add login test scripts and global variables.

---
+++ tests/omero_connector/test__002__login-wrong-username.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+set -e 
+
+source global_variables.sh
+
+$CONNECTOR_CALL_USER_WRONG checkCredentials
---
+++ tests/omero_connector/test__003__login-wrong-password.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+set -e 
+
+source global_variables.sh
+
+$CONNECTOR_CALL_PW_WRONG checkCredentials
