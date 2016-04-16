5af2d5520837421c6f940b651c0b9b5722867ea1
Aaron Ponti <aarpon@gmail.com>
2016-04-16 17:17:05 +0200

    Add setup scripts for devel vs. release.

---
+++ setup/setup_devel.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+#
+# Set project directory
+#
+PROJECT_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/../
+
+###############################################################################
+#
+# Composer
+#
+###############################################################################
+
+echo "Set up HRM for development..."
+
+# Update composer itself
+${PROJECT_DIR}/composer.phar self-update
+
+# Make sure all third-party dependencies exist and are up-to-date
+if [ ! -d "${PROJECT_DIR}/vendor" ]; then
+    # Install
+    ${PROJECT_DIR}/composer.phar install
+else
+    # Update
+    ${PROJECT_DIR}/composer.phar update
+fi
+
--- setup/setup_release.sh
+++ setup/setup_release.sh
@@ -11,15 +11,17 @@ PROJECT_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/../
 #
 ###############################################################################
 
+echo "Set up HRM for release..."
+
 # Update composer itself
 ${PROJECT_DIR}/composer.phar self-update
 
 # Make sure all third-party dependencies exist and are up-to-date
 if [ ! -d "${PROJECT_DIR}/vendor" ]; then
     # Install
-    ${PROJECT_DIR}/composer.phar install
+    ${PROJECT_DIR}/composer.phar install --no-dev
 else
     # Update
-    ${PROJECT_DIR}/composer.phar update
+    ${PROJECT_DIR}/composer.phar update --no-dev
 fi
 
