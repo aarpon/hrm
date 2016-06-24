286c0c11c81553dc2edf6bf354a73b1ea7249a11
Niko Ehrenfeuchter <nikolaus.ehrenfeuchter@unibas.ch>
2016-06-24 12:31:41 +0200

    Print warning if 'cur' is not empty instead of refusing to start up.
    refs #514, #516

--- lib/python/HRM.py
+++ lib/python/HRM.py
@@ -120,18 +120,25 @@ def setup_rundirs(base_dir):
                     raise OSError("Error creating Queue Manager runtime "
                         "directory '%s': %s" % (cur, err))
             full_subdirs[sub_dir] = cur
+
     # pick up any existing jobfiles in the 'new' spooldir
     full_subdirs['newfiles'] = list()
     new_existing = os.listdir(full_subdirs['new'])
-    if len(new_existing) > 0:
+    if new_existing:
         for fname in new_existing:
             logw("Found existing file in 'new' directory: %s" % fname)
             full_subdirs['newfiles'].append(fname)
     logi("Runtime directories:\n%s" % pprint.pformat(full_subdirs))
-    # TODO: define how to deal with existing files in the 'cur' directory!
+
+    # check the 'cur' directory and issue a warning only if non-empty:
     cur_existing = os.listdir(full_subdirs['cur'])
-    if len(cur_existing) > 0:
-        raise RuntimeError("Directory non-empty: %s" % full_subdirs['cur'])
+    if cur_existing:
+        logw("%s WARNING %s", "=" * 60, "=" * 60)
+        logw("Spooling directory '%s' non-empty, this could be due to an "
+             "unclean shutdown of the Queue Manager!", full_subdirs['cur'])
+        for fname in cur_existing:
+            logw("- file: %s", os.path.join(full_subdirs['cur'], fname))
+        logw("%s WARNING %s", "=" * 60, "=" * 60)
     return full_subdirs
 
 
