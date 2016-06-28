67c2848b568a509799f556c14c63b77364ae0458
Niko Ehrenfeuchter <nikolaus.ehrenfeuchter@unibas.ch>
2016-06-28 11:36:39 +0200

    More basic testing infrastructure: add tests runner script.

---
+++ tests/gc3qm/run_tests.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+set -e
+# remember path prefix as called, source functions
+PFX=$(dirname $0)
+cd $PFX
+source "functions.inc.sh"
+set +e
+
+for TEST in test-*__*.sh ; do
+    # parse the "short" test name (basically the number):
+    SHORT=$(echo $TEST | sed 's,__.*,,')
+    RES="results/$SHORT"
+    set -e
+    rm -rf $RES
+    mkdir -p $RES
+    set +e
+    echo "++++++++++++++++++++ Running $SHORT ($TEST) ++++++++++++++++++++"
+    STDOUT="$RES/stdout"
+    STDERR="$RES/stderr"
+    EXITVAL="$RES/exitval"
+    bash $TEST >$STDOUT 2>$STDERR
+    RET=$?
+    echo $RET > $EXITVAL
+    echo "Test '$SHORT' finished (exit code: $RET, results in '$PFX/$RES')."
+done
