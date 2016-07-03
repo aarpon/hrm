2910a1b4a0dd53b13cda4982030340b6839aba20
Niko Ehrenfeuchter <nikolaus.ehrenfeuchter@unibas.ch>
2016-07-04 00:50:18 +0200

    Add test + results for a long running job being killed.
    WARNING: this leaves back garbage in the gc3pie resource directory (by
    default ~/.gc3/shellcmd.d/) which prevents the QM from starting up next
    time.

---
+++ tests/gc3qm/test-004__decon-job-long_kill-at-10s.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+# exit on any error:
+set -e
+
+# remember path prefix as called, source functions
+PFX=$(dirname $0)
+source "$PFX/functions.inc.sh"
+
+SHORT=$(parse_shortname)
+
+########## TEST DESCRIPTION ##########
+# intended behaviour:
+# 1) start the QM
+# 2) switch to pause mode
+# 3) place the jobs from the inputs directory in the queue
+# 4) switch to run mode
+# 5) shutdown QM when queue is empty, latest after 5 SECONDS
+########## TEST DESCRIPTION ##########
+
+
+startup_qm
+
+qm_request pause
+sleep 1
+
+# we are in the HRM base dir now, so use the full path for job files:
+for jobfile in tests/gc3qm/inputs/$SHORT/*.cfg ; do
+    cp -v $jobfile "$QM_SPOOL/spool/new"
+    sleep .1
+done
+
+sleep .5
+
+qm_request run
+sleep 1
+
+shutdown_qm_on_empty_queue 5
+
+msg_finished
+
