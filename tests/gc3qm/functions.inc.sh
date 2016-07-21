4846ec550ddb8aa0aa4c125f5b308d99e0a2d832
Niko Ehrenfeuchter <nikolaus.ehrenfeuchter@unibas.ch>
2016-07-21 18:00:26 +0200

    More timestamp stripping.

--- tests/gc3qm/functions.inc.sh
+++ tests/gc3qm/functions.inc.sh
@@ -200,5 +200,5 @@ strip_runtime_strings() {
     # better comparable among subsequent individual runs
     sed -s 's/[0-9a-f]\{40\}/UID_STRIPPED/g' |
     sed -s 's/App@[0-9a-f]\{12\}/App@APPID_STRIPPED/g' |
-    sed -s 's/[0-9]\{10\}\.[0-9]\{5,6\}/TIMESTAMP_STRIPPED/g'
+    sed -s 's/[0-9]\{10\}\.[0-9]\{4,6\}/TIMESTAMP_STRIPPED/g'
 }
