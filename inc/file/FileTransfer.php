2dedd4a20c3118146a76072405686524eb8189e0
felix cc <felix.meyenhofer@unifr.ch>
2017-07-31 11:22:17 +0200

    Add place-holder class for the up/download stuff

---
+++ inc/file/FileTransfer.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Class FileServer
+ *
+ * @package hrm
+ *
+ * This file is part of the Huygens Remote Manager
+ * Copyright and license notice: see license.txt
+ */
+
+namespace hrm\file;
+
+/**
+ * Class FileTransfer
+ * @package hrm\file
+ *
+ * class for file up-and downloads
+ *
+ * @todo get the stuff form Fileserver, FileserverV2, Util and UtilV2
+ */
+class FileTransfer
+{
+
+}
