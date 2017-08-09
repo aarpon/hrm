36b1094523f92d96e64c1df2dbaa3623648e7b37
Daniel Sevilla <daniel@svi.nl>
2017-08-09 12:51:12 +0200

    Begin basic navitation acrros microscopic parameter pages.
    - To be simplified via JavaScript in one single page later on.

--- image_format_new.php
+++ image_format_new.php
@@ -19,6 +19,8 @@ require_once dirname(__FILE__) . '/inc/bootstrap.php';
 
 session_start();
 
+$message = "";
+
 if (!isset($_SESSION['user']) || !$_SESSION['user']->isLoggedIn()) {
     header("Location: " . "login.php");
     exit();
@@ -33,13 +35,52 @@ if (!isset($_SESSION['setting'])) {
     $_SESSION['setting'] = new ParameterSetting();
 }
 
-$message = "";
+/* *****************************************************************************
+ *
+ * SET THE CONFIDENCES FOR THE RELEVANT PARAMETERS
+ *
+ **************************************************************************** */
 
-// The Twig handler.
+/*
+   In this page, all parameters are required and independent of the
+   file format chosen!
+*/
+$parameterNames = $_SESSION['setting']->imageParameterNames();
+$db = new DatabaseConnection();
+foreach ($parameterNames as $name) {
+    $parameter = $_SESSION['setting']->parameter($name);
+    $confidenceLevel = $db->getParameterConfidenceLevel('', $name);
+    $parameter->setConfidenceLevel($confidenceLevel);
+    $_SESSION['setting']->set($parameter);
+}
+
+/* *****************************************************************************
+ *
+ * PROCESS THE POSTED PARAMETERS
+ *
+ **************************************************************************** */
+
+if ($_SESSION['setting']->checkPostedImageParameters($_POST)) {
+
+    // Now we force all variable channel parameters to have the correct number
+    // of channels
+    $_SESSION['setting']->setNumberOfChannels(
+        $_SESSION['setting']->numberOfChannels());
+
+    // Continue to the next page
+    header("Location: " . "microscope_parameter.php");
+    exit();
+} else {
+    $message = $_SESSION['setting']->message();
+}
+
+/* *****************************************************************************
+ *
+ * THE HTML PAGE VIA TWIG
+ *
+ **************************************************************************** */
+
 $loader = new Twig_Loader_Filesystem('templates');
 $twig = new Twig_Environment($loader);
-
-/* Render the HTML code. */
-//echo $twig->render('home.twig', array('isAdmin' => $_SESSION['user']->isAdmin()));
-
-echo $twig->render('image_parameters.twig');
+echo $twig->render('image_parameters.twig',
+                   array('NumberOfChannels' => $_SESSION['setting']->numberOfChannels()));
---
+++ microscope_parameter_new.php
@@ -0,0 +1,45 @@
+<?php
+// This file is part of the Huygens Remote Manager
+// Copyright and license notice: see license.txt
+
+use hrm\DatabaseConnection;
+use hrm\Nav;
+use hrm\param\base\Parameter;
+use hrm\setting\ParameterSetting;
+use hrm\Util;
+
+require_once dirname(__FILE__) . '/inc/bootstrap.php';
+
+
+/* *****************************************************************************
+ *
+ * START SESSION, CHECK LOGIN STATE, INITIALIZE WHAT NEEDED
+ *
+ **************************************************************************** */
+
+session_start();
+
+if (!isset($_SESSION['user']) || !$_SESSION['user']->isLoggedIn()) {
+    header("Location: " . "login.php");
+    exit();
+}
+
+if (isset($_GET['home'])) {
+    header("Location: " . "home.php");
+    exit();
+}
+
+if (!isset($_SESSION['setting'])) {
+    $_SESSION['setting'] = new ParameterSetting();
+}
+
+$message = "";
+
+// The Twig handler.
+$loader = new Twig_Loader_Filesystem('templates');
+$twig = new Twig_Environment($loader);
+
+/* Render the HTML code. */
+//echo $twig->render('home.twig', array('isAdmin' => $_SESSION['user']->isAdmin()));
+
+echo $twig->render('microscope_parameter.twig');
