9f5f6dfa23a978cb95d8caa43836cc7cfe33eee8
Aaron Ponti <aaron.ponti@bsse.ethz.ch>
2018-12-14 14:41:40 +0100

    Add dark theme to add_user_popup.php.

--- add_user_popup.php
+++ add_user_popup.php
@@ -158,7 +158,18 @@ if (isset($_POST['add'])) {
         -->
     </script>
     <style type="text/css">
-        @import "css/default.css?v=3.5";
+        <?php
+        global $switch_to_dark_mode;
+        if ($switch_to_dark_mode == true) {
+        ?>
+        @import "css/dark.css?v=3.6";
+        <?php
+        } else {
+        ?>
+        @import "css/default.css?v=3.6";
+        <?php
+        }
+        ?>
     </style>
 </head>
 
