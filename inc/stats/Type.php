e4ab9564378be0a0a9fbbff9122ef4f81ca36094
Daniel Sevilla <daniel@svi.nl>
2018-12-17 15:22:46 +0100

    Slightly better code for the statistics script.

--- inc/stats/Type.php
+++ inc/stats/Type.php
@@ -214,38 +214,29 @@ class Type
         // Create script
         // -------------------------------------------------------------------------
         if ($switch_to_dark_mode) {
-            $script = "$(document).ready(function() {
+            $labelColor = 'white';
+            $mainBgColor = '#1e1e1e';
+        } else {
+            $labelColor = 'black';
+            $mainBgColor = 'white';
+        }
+
+        $script = "$(document).ready(function() {
             var chart = new Highcharts.Chart({
-            chart: { renderTo: 'statschart', margin: [50, 200, 60, 170], backgroundColor: '#1e1e1e' },
+            chart: { renderTo: 'statschart', margin: [50, 200, 60, 170], backgroundColor: '" . $mainBgColor . "' },
             title: { text: '" . $title . "'  },
             subtitle: { text: '" . $subtitle . "' },
             plotArea: { shadow: null, borderWidth: null, backgroundColor: null },
             tooltip: { formatter: function() { return '<b>'+ this.point.name +'</b>: '+ this.y +' %'; } },
             plotOptions: { pie: { dataLabels: { enabled: true,
                                             formatter: function() { if (this.y > 5) return this.point.name; },
-                                            color: 'white',
+                                            color: '" . $labelColor . "',
                                             style: { font: '13px Trebuchet MS, Verdana, sans-serif' } } } },
 	        legend: { layout: 'vertical', style: { left: 'auto', bottom: 'auto', right: '50px', top: '100px' } },
             series: [{ type: 'pie', name: '" . $title . "', data: " . $data . " } ] });
 	    	});
             ";  
-        } else {
-            $script = "$(document).ready(function() {
-                var chart = new Highcharts.Chart({
-                chart: { renderTo: 'statschart', margin: [50, 200, 60, 170]},
-                title: { text: '" . $title . "'  },
-                subtitle: { text: '" . $subtitle . "' },
-                plotArea: { shadow: null, borderWidth: null, backgroundColor: null },
-                tooltip: { formatter: function() { return '<b>'+ this.point.name +'</b>: '+ this.y +' %'; } },
-                plotOptions: { pie: { dataLabels: { enabled: true,
-                                                formatter: function() { if (this.y > 5) return this.point.name; },
-                                                color: 'black',
-                                                style: { font: '13px Trebuchet MS, Verdana, sans-serif' } } } },
-                legend: { layout: 'vertical', style: { left: 'auto', bottom: 'auto', right: '50px', top: '100px' } },
-                series: [{ type: 'pie', name: '" . $title . "', data: " . $data . " } ] });
-                });
-                ";
-        }
+        
         return $script;
     }
 
