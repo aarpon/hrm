3dc73c245063c75dde6c084d1783dd749bda4a6f
Aaron Ponti <aarpon@gmail.com>
2021-07-22 15:15:51 +0200

    Increase version to 3.7.1.

--- inc/System.php
+++ inc/System.php
@@ -42,7 +42,7 @@ class System
      * developers!
      * @var int
      */
-    private const HRM_VERSION_MAINTENANCE = 0;
+    private const HRM_VERSION_MAINTENANCE = 1;
 
     /**
      * Database revision needed by current HRM version. This value has to be
