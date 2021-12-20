0cbf73256ad081f0b010a96edb4f40ca09941901
Daniel Sevilla <daniel@svi.nl>
2021-12-20 17:43:54 +0100

    Add a short explanation about the image selection buttons to the Quick Help.
    - #731.

--- select_images.php
+++ select_images.php
@@ -68,7 +68,13 @@ include("header.inc.php");
 $info = "<h3>Quick help</h3>" .
     "<p>Here you can select the files to be restored from the list " .
     "of available images. The file names are filtered by the selected " .
-    "file format. Use SHIFT- and CTRL-click to select multiple files.</p>" .
+    "file format." .
+    "<p>Use SHIFT- and CTRL-click to select multiple files from any of the boxes.</p>" .
+    "<p>Use the arrow buttons " .
+    "<img src=\"images/add.png\" alt=\"Help\" width=\"22\" height=\"22\"/>" .
+    "<img src=\"images/remove.png\" alt=\"Help\" width=\"22\" height=\"22\"/>" .
+    "to add/remove images to the <b><i>Selected Images</i></b> box from the " .
+    "<b><i>Images available on server</i></b> box.</p>" .
     "<p>Where applicable, the files belonging to a series can be condensed " .
     "into one file name by checking the 'autoseries' option. These files " .
     "will be loaded and deconvolved as one large dataset. Unchecking " .
