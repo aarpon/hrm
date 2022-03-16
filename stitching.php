77e223065871ddd6e34739a0ce4ed227e52b4b1f
Daniel Sevilla <daniel@svi.nl>
2022-03-16 17:13:16 +0100

    Add basic controls, footer and quickhelp.

--- stitching.php
+++ stitching.php
@@ -11,7 +11,19 @@ require_once dirname(__FILE__) . '/inc/bootstrap.php';
 
 
 
+/* *****************************************************************************
+ *
+ * START SESSION, CHECK LOGIN STATE, INITIALIZE WHAT NEEDED
+ *
+ **************************************************************************** */
 
+session_start();
+
+if (!isset($_SESSION['user']) || !$_SESSION['user']->isLoggedIn()) {
+    header("Location: " . "login.php");
+    exit();
+}
+$message = "";
 
 
 
@@ -48,5 +60,120 @@ if ($_SESSION['task_setting']->checkPostedTaskParameters($_POST)) {
 }
 
 
+/* *****************************************************************************
+ *
+ * CREATE THE PAGE
+ *
+ **************************************************************************** */
 
+// Javascript includes
+$script = array("settings.js", "quickhelp/help.js",
+    "quickhelp/postProcessingHelp.js");
+
+include("header.inc.php");
+?>
+
+
+<!--
+  Tooltips
+-->
+<span class="toolTip" id="ttSpanCancel">
+        Abort editing and go back to the Restoration parameters
+        selection page. All changes will be lost!
+    </span>
+<span class="toolTip" id="ttSpanSave">
+        Save and return to the processing parameters selection page.
+    </span>
+<span class="toolTip" id="ttSpanBack">
+        Go back to previous page.
+    </span>
+
+<div id="nav">
+    <div id="navleft">
+        <ul>
+            <?php
+            echo(Nav::linkWikiPage('HuygensRemoteManagerHelpRestorationParameters'));
+            ?>
+            <li> [ <?php echo $_SESSION['task_setting']->name(); ?> ]</li>
+        </ul>
+    </div>
+    <div id="navright">
+        <ul>
+            <?php
+            echo(Nav::textUser($_SESSION['user']->name()));
+            ?>
+        </ul>
+    </div>
+    <div class="clear"></div>
+</div>
+
+
+<div id="content">
+    <h3>Restoration - Stitching</h3>
+
+    <form method="post" action="" id="stitch">
+
+
+
+
+                                    
+                          
+    <div><input name="OK" type="hidden"/></div>
+
+    <div id="controls"
+         onmouseover="changeQuickHelp( 'default' )">
+        <input type="button" value="" class="icon previous"
+               onmouseover="TagToTip('ttSpanBack' )"
+               onmouseout="UnTip()"
+               onclick="document.location.href='task_parameter.php'"/>
+        <input type="button" value="" class="icon up"
+               onmouseover="TagToTip('ttSpanCancel' )"
+               onmouseout="UnTip()"
+               onclick="deleteValuesAndRedirect(
+                    'select_task_settings.php' );"/>
+        <input type="submit" value="" class="icon next"
+               onmouseover="TagToTip('ttSpanForward' )"
+               onmouseout="UnTip()"
+               onclick="process()"/>
+    </div>
+
+    </form>                          
+</div> <!-- content -->
+                               
+
+
+
+
+<div id="rightpanel" onmouseover="changeQuickHelp( 'default' )">
+    <div id="info">
+        <h3>Quick help</h3>
+        <div id="contextHelp">
+            <p>On this page you specify the parameters of those
+               restoration operations to be carried out on the deconvolved
+               image.</p>
+        </div>
+    </div>
+
+    <div id="message">
+        <?php
+
+        echo "<p>$message</p>";
+
+        ?>
+    </div>
+
+</div> <!-- rightpanel -->
+
+
+<?php      
+include("footer.inc.php");
+
+// Workaround for IE
+if (Util::using_IE() && !isset($_SERVER['HTTP_REFERER'])) {
+    ?>
+    <script type="text/javascript">
+        $(document).ready(retrieveValues());
+    </script>
+    <?php
+}
 ?>
